f0b8bdfa12f463099626791582ef607bdeb7e433
SandeepKumar <sandeepkusolanki@gamil.com>
2025-06-05 18:19:59 +0530

    changes in CryptoService api calling

--- src/features/crypto/CryptoService.js
+++ src/features/crypto/CryptoService.js
@@ -7,45 +7,21 @@ const headers = {
 
 export const getGlobalStats = async () => {
   const res = await axios.get(`https://${key}/stats`, { headers });
-  // console.log("getgloabalstats",res.data.data);
+ 
   return res.data.data;
   
 };
 
 export const getCryptos = async () => {
   const res = await axios.get(`https://${key}/coins`, { headers });
-  // console.log("getCryptos ",res.data.data.coins);
   return res.data.data.coins;
   
 };
 
 export const getCryptoDetail = async (id) => {
-  const res = await axios.get(`https://${key}coinranking1.p.rapidapi.com/coin/${id}`, { headers });
-  // console.log("getCryptoDetail : - ",res.data.data.coin);
+  const res = await axios.get(`https://${key}/coin/${id}`, { headers });
   return res.data.data.coin;
-  // return res.data.data.coin;
   
 };
 
-// export const getCryptoDetail = async (id) => {
-//   if (!id) {
-//     console.error("Missing crypto ID");
-//     return null;
-//   }
 
-//   try {
-//     const res = await axios.get(`https://coinranking1.p.rapidapi.com/coin/${id}`, {
-//       headers,
-//       params: {
-//         referenceCurrencyUuid: 'yhjMzLPhuIDl', // USD
-//         timePeriod: '24h'
-//       }
-//     });
-//     console.log("getCryptoDetail:", res.data.data.coin);
-//     return res.data.data.coin;
-//   } catch (error) {
-//     console.error("Error in getCryptoDetail:", error.response?.data || error.message);
-//     return null;
-//   }
-// };
-// const BASE_URL = 'https://coinranking1.p.rapidapi.com';
